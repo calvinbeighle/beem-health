8f412181147a1ec2d6f76ff169c7a9a4a762d62f
calvinbeighle <calvinbeighle@college.harvard.edu>
2024-11-27 20:59:20 -0800

    Delete beem-health-dashboard directory

--- beem-health-dashboard/app/styles/fonts.ts
+++
@@ -1,8 +0,0 @@
-import localFont from 'next/font/local'
-
-export const emberlyBold = localFont({ 
-  src: '../fonts/Emberly-Bold.woff2',
-  variable: '--font-emberly-bold',
-  display: 'swap',
-})
-
